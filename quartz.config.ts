1decf60784b20677987b45fa7547b95b7ae15d01
Esra <esrametin.7@gmail.com>
2024-04-14 14:43:38 +0300

    Quartz sync: Apr 14, 2024, 2:43 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,7 +22,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Alegreya",
+        header: "Spectral",
         body: "Spectral",
         code: "IBM Plex Mono",
       },
